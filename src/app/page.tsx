534d79d7178a59a877b7036ed11c399aa28fa342
jsb7131 <jasonstephenbaker@gmail.com>
2024-12-10 22:08:53 -0500

    Fixing typo in comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 
-// Fallback page to redirect to the historic districts map. Rediirect is handled in next.config.ts
+// Fallback page to redirect to the historic districts map. The redirect is actually handled in next.config.ts
 export default function Home() {
   redirect("/nola-historic-districts");
 }
